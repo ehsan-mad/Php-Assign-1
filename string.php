2d86971a701a953b4b4d0eb0853ee8d01c37bd52
ehsan-mad <saadkhan420000@gmail.com>
2025-01-01 22:53:01 +0600

    first commit

---
+++ string.php
@@ -0,0 +1,22 @@
+
+<?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+
+function countvowel($str){
+    $vowel=['a','e','i','o','u','A','E','I','O','U'];
+    $count=0;
+    for ($i=0;$i<strlen($str);$i++){
+        if(in_array($str[$i],$vowel)){
+            $count++;
+        }
+    }
+    return $count;
+}
+
+foreach($strings as $str){
+    $revstring=strrev($str);
+    $Vowelcount=countvowel($str);
+    echo "Orginal String $str , Vowel Count: $Vowelcount , Reversed String: $revstring \n";
+
+}
